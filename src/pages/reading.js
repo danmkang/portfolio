241c3f6b87d19d7c5ca1ab77e70d48343c16c812
danmkang <danminkang@gmail.com>
2020-05-24 23:26:26 -0400

    Upload Site

---
+++ src/pages/reading.js
@@ -0,0 +1,33 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function Reading() {
+  return (
+    <Layout>
+      <h2>
+        Reading Recommendations 📗    
+      </h2>
+      <p>
+        My rules when it comes to books are simple. If a book is boring after a few pages, I stop reading it. 
+        I try to switch between fiction and non-fiction after each read. 
+      </p> 
+      <p>
+      Here are the books I've enjoyed in the past, in no particular order:
+      </p>
+      <ul>
+        <li><b>Slouching Towards Bethlehem</b> by Joan Didion</li>
+        <li><b>The Splendid and the Vile: A Saga of Churchill, Family, and Defiance During the Blitz</b> by Erik Larsen</li>
+        <li><b>Miracles of the Namiya General Store</b> by Keigo Higashino (translated to Korean)</li>
+        <li><b>The Foundation Trilogy</b> by Isaac Asimov</li>
+        <li><b>Permanent Record</b> by Edward Snowden</li>
+        <li><b>Adults in the Room: My Battle with the European and American Deep Establishment</b> by Yanis Varoufakis</li>
+        <li><b>The Forever War</b> by Joe Haldeman</li>
+        <li><b>Team of Rivals</b> by Doris Kearns Goodwind</li>
+        <li><b>Stories of Your Life and Others</b> by Ted Chiang</li>
+        <li><b>In Cold Blood</b> by Truman Capote</li>
+        <li><b>Kitchen Confidential</b> by Anthony Bourdain</li>
+        <li><b>The Autobiography of Malcolm X</b> by Malcolm X</li>
+      </ul>
+  </Layout>
+  )
+}
